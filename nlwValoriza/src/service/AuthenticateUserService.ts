b74afba40d65743748da7761711015e3e2d37749
JoaoVitorFarias <jvlopesfarias@gmail.com>
2021-06-25 22:31:47 -0300

    Add user authentication

---
+++ nlwValoriza/src/service/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({
+      email,
+    });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const token = sign(
+      {
+        email: user.email,
+      },
+      "43d647c147b341639a4276be4851df27",
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return token;
+  }
+}
+//43d647c147b341639a4276be4851df27
+
+export { AuthenticateUserService };
